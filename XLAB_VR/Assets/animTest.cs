efc0a96bbb13c578f6f1cf599e46f170542730c9
XlabAR <43551630+XlabAR@users.noreply.github.com>
2018-12-07 00:29:09 -0500

    Set up tree to animate in test scene
    Saved as prefab

---
+++ XLAB_VR/Assets/animTest.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class animTest : MonoBehaviour
+
+{
+
+    public AudioClip hitSound;
+
+    private Animator anim;
+
+    private AudioSource theAudio;
+
+  
+
+    void Start()
+
+    {
+
+        anim = GetComponent<Animator>();
+
+        theAudio = GetComponent<AudioSource>();
+
+        anim.SetBool("hit", false);
+
+    }
+
+ void OnCollisionEnter(Collision col)
+
+    {
+			Debug.Log ("collision detected");
+	
+            StartCoroutine(TriggerAnimatorBool("hit"));
+
+            theAudio.PlayOneShot(hitSound, 0.9f);
+
+    }
+
+    private IEnumerator TriggerAnimatorBool(string name)
+
+    {
+
+        anim.SetBool(name, true);
+
+        yield return null;
+
+    }
+
+}
